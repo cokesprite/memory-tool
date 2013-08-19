fd4d17610f939ef26755fe56ecf9df0677db9e9a
Chun Chen <chun.chen@archermind.com>
2013-08-19 11:31:52 +0800

    Modify some paragraph text and chart data
    1. Add 'Free' and 'Used' data according to:
        Free = MemFree + Cached + SwapCached - Mlocked - Shmem
        Used = AnonPages + Slab + VmallocAlloc + Mlocked + Shmem + KernelStack + PageTables + KGSL_ALLOC + ION_ALLOC
    2. Change some text and style
    
    Signed-off-by: Chun Chen <chun.chen@archermind.com>

--- mmparse.py
+++ mmparse.py
@@ -16,7 +16,8 @@ from reportlab.platypus.tables import TableStyle
 
 (PAGE_WIDTH, PAGE_HEIGHT) = defaultPageSize
 Styles = {'Normal': ParagraphStyle(name='Normal', fontName='Helvetica', fontSize=10, leading=12),
-          'Header1': ParagraphStyle(name='Header', fontName='Helvetica', fontSize=12, leading=12),}
+          'Tips': ParagraphStyle(name='Header', fontName='Helvetica', fontSize=8, leading=12),
+          'Header': ParagraphStyle(name='Header', fontName='Helvetica-Bold', fontSize=12, leading=12),}
 
 class Meminfo(object):
     def __init__(self, filePath):
@@ -28,15 +29,30 @@ class Meminfo(object):
         fileObject = open(self.filePath, 'r').read()
         print '---> Start parsing meminfo memlog ...'
         chunks = fileObject.split('------ MEMORY INFO')
+        dates = []
         for chunk in chunks[1:]:
             lines = chunk.split('\n')
-            date = re.search('\d\d\d\d-\d\d-\d\d \d\d:\d\d:\d\d', lines[0])
+            date = re.search('\d\d\d\d-\d\d-\d\d \d\d:\d\d:\d\d', lines[0]).group()
+            dates.append(date)
             for i in range(len(lines[1:-2])):
                 formated = lines[1:-2][i].split()
+                if len(formated) == 0: break
                 if not self.meminfo.has_key(formated[0][:-1]):
                     self.meminfo[formated[0][:-1]] = []
-                self.meminfo[formated[0][:-1]].append([formated[1], date.group()])
+                self.meminfo[formated[0][:-1]].append([formated[1], date])
+            if not self.meminfo.has_key('Free'):
+                self.meminfo['Free'] = []
+            if not self.meminfo.has_key('Used'):
+                self.meminfo['Used'] = []
+            self.meminfo['Free'].append([int(self.meminfo['MemFree'][-1][0]) + int(self.meminfo['Cached'][-1][0]) + int(self.meminfo['SwapCached'][-1][0]) - int(self.meminfo['Mlocked'][-1][0]) - int(self.meminfo['Shmem'][-1][0]), date])
+            self.meminfo['Used'].append([int(self.meminfo['AnonPages'][-1][0]) + int(self.meminfo['Slab'][-1][0]) + int(self.meminfo['VmallocAlloc'][-1][0]) + int(self.meminfo['Mlocked'][-1][0]) + int(self.meminfo['Shmem'][-1][0]) + int(self.meminfo['KernelStack'][-1][0]) + int(self.meminfo['PageTables'][-1][0]) + int(self.meminfo['KGSL_ALLOC'][-1][0]) + int(self.meminfo['ION_ALLOC'][-1][0]), date])
         print '---> Done'
+
+        for i in range(len(dates)):
+            if int(self.meminfo['Free'][i][0]) != (int(self.meminfo['MemFree'][i][0]) + int(self.meminfo['Cached'][i][0]) + int(self.meminfo['SwapCached'][i][0]) - int(self.meminfo['Mlocked'][i][0]) - int(self.meminfo['Shmem'][i][0])):
+                print '!!!!!!!!!!!!!!!!!!!!!!!!!!!!'
+            if int(self.meminfo['Used'][i][0]) != (int(self.meminfo['AnonPages'][i][0]) + int(self.meminfo['Slab'][i][0]) + int(self.meminfo['VmallocAlloc'][i][0]) + int(self.meminfo['Mlocked'][i][0]) + int(self.meminfo['Shmem'][i][0]) + int(self.meminfo['KernelStack'][i][0]) + int(self.meminfo['PageTables'][i][0]) + int(self.meminfo['KGSL_ALLOC'][i][0]) + int(self.meminfo['ION_ALLOC'][i][0])):
+                print '!!!!!!!!!!!!!!!!!!!!!!!!!!!!'
 
     def drawingData(self, items, title=None):
         data = []
@@ -67,6 +83,7 @@ class Procrank(object):
                 self.ram = reduce(lambda x, y: y if x <= (int(ram.group()) / 1000) and y > (int(ram.group()) / 1000) else x, self.RAMS.keys())
             for line in lines[2:-5]:
                 formated = line.split()
+                if len(formated) == 0: break
                 if not self.procrank.has_key(formated[-1]):
                     self.procrank[formated[-1]] = []
                 self.procrank[formated[-1]].append(formated[1:-1] + [date])
@@ -85,14 +102,10 @@ class Procrank(object):
         return sorted(filter(lambda proc: max([int(value[2][:-1]) for value in self.procrank[proc]]) < 1000 * self.RAMS[self.ram][0], self.procrank.keys()), key=lambda proc: max([int(value[2][:-1]) for value in self.procrank[proc]]), reverse=True)[:10]
 
     def hotProcs(self):
-        return filter(lambda x: self.procrank.has_key(x), ['com.htc.launcher', 'surfaceflinger', 'system_server', 'com.android.browser', 'android.process.acore',
-                'com.android.phone', 'com.android.systemui', 'com.htc.idlescreen.shortcut', 'com.android.chrome',
-                'com.android.launcher', 'com.android.htcdialer', 'com.htc.android.htcime'])
+        return filter(lambda x: self.procrank.has_key(x), ['com.htc.launcher', 'surfaceflinger', 'system_server', 'com.android.browser', 'android.process.acore', 'com.android.phone', 'com.android.systemui', 'com.htc.idlescreen.shortcut', 'com.android.chrome', 'com.android.launcher', 'com.android.htcdialer', 'com.htc.android.htcime'])
 
     def tableData(self, procs):
-        return [['Process Name', 'RSS Average (KB)', 'RSS Peak (KB)', 'PSS Average (KB)', 'PSS Peak (KB)']] + [[proc,
-                reduce(lambda x, y: x + y, [int(value[1][:-1]) for value in self.procrank[proc]]) / len(self.procrank[proc]),
-                max([int(value[1][:-1]) for value in self.procrank[proc]]),
+        return [['Process Name', 'PSS Average (KB)', 'PSS Peak (KB)']] + [[proc,
                 reduce(lambda x, y: x + y, [int(value[2][:-1]) for value in self.procrank[proc]]) / len(self.procrank[proc]),
                 max([int(value[2][:-1]) for value in self.procrank[proc]])] for proc in procs]
 
@@ -200,32 +213,46 @@ class PDFGen(object):
 
     def generate(self, meminfo, procrank):
         doc = SimpleDocTemplate('memory analysis report.pdf')
-        style = Styles['Normal']
         story = [Spacer(1, 1.7 * inch)]
         story.append(PageBreak())
+        print '---> Start drawing chart of items in meminfo ...'
+        story.append(Spacer(1, 0.5 * inch))
+        story.append(Paragraph('Meminfo Analysis', Styles['Header']))
+        story.append(Spacer(1, 0.5 * inch))
+        story.append(self.drawLineChart(meminfo.drawingData(['Free', 'Cached', 'AnonPages', 'Used'])))
+        story.append(Spacer(1, 0.5 * inch))
+        story.append(Paragraph('* Free = MemFree + Cached + SwapCached - Mlocked - Shmem', Styles['Tips']))
+        story.append(Paragraph('* Used = AnonPages + Slab + VmallocAlloc + Mlocked + Shmem + KernelStack + PageTables + KGSL_ALLOC + ION_ALLOC', Styles['Tips']))
+        story.append(Spacer(1, 0.5 * inch))
+        story.append(self.drawLineChart(meminfo.drawingData(['Buffers', 'Mlocked', 'Shmem', 'Slab', 'KernelStack', 'PageTables', 'VmallocAlloc', 'ION_ALLOC'])))
+        story.append(Spacer(1, 0.5 * inch))
+        print '---> Done'
         print '---> Start drawing table of average and peak procrank items ...'
-        story.append(Paragraph('Average and Peak RSS/PSS in Procrank (top 20 of memory usage)', style))
+        story.append(PageBreak())
+        story.append(Paragraph('Procrank Analysis', Styles['Header']))
+        story.append(Spacer(1, 0.5 * inch))
+        story.append(Paragraph('Top 20 Procrank', Styles['Normal']))
         story.append(Spacer(1, 0.1 * inch))
         story.append(self.drawTable(procrank.tableData(procrank.topProcs())))
         story.append(Spacer(1, 0.5 * inch))
-        story.append(Paragraph('Average and Peak RSS/PSS in Procrank (pre-defined applications)', style))
+        story.append(Paragraph('Pre-defined Applications', Styles['Normal']))
         story.append(Spacer(1, 0.1 * inch))
         story.append(self.drawTable(procrank.tableData(procrank.hotProcs())))
         print '---> Done'
-        print '---> Start drawing chart of items in meminfo ...'
-        story.append(Spacer(1, 0.5 * inch))
-        story.append(self.drawLineChart(meminfo.drawingData(['MemFree', 'Cached', 'AnonPages'], 'Meminfo First Group')))
-        story.append(Spacer(1, 0.5 * inch))
-        story.append(self.drawLineChart(meminfo.drawingData(['Buffers', 'Mlocked', 'Shmem', 'Slab', 'KernelStack', 'PageTables', 'VmallocAlloc', 'ION_ALLOC'], 'Meminfo Second Group')))
-        story.append(Spacer(1, 0.5 * inch))
-        print '---> Done'
         print '---> Start drawing chart of top processes in procrank ...'
-        story.append(self.drawLineChart(procrank.drawingData(procrank.peakHighProcs(), 'Peak >= 100MB')))
-        story.append(Spacer(1, 0.5 * inch))
-        story.append(self.drawLineChart(procrank.drawingData(procrank.peakMediumProcs(), '50MB <= Peak < 100MB')))
-        story.append(Spacer(1, 0.5 * inch))
-        story.append(self.drawLineChart(procrank.drawingData(procrank.peakLowProcs(), 'Peak < 50MB')))
-        story.append(Spacer(1, 0.5 * inch))
+        story.append(PageBreak())
+        story.append(Paragraph('PSS Peak Above %s MB' % procrank.RAMS[procrank.ram][1], Styles['Normal']))
+        story.append(Spacer(1, 0.1 * inch))
+        story.append(self.drawLineChart(procrank.drawingData(procrank.peakHighProcs())))
+        story.append(Spacer(1, 0.6 * inch))
+        story.append(Paragraph('PSS Peak in (%s MB, %s MB)' % (procrank.RAMS[procrank.ram][0], procrank.RAMS[procrank.ram][1]), Styles['Normal']))
+        story.append(Spacer(1, 0.1 * inch))
+        story.append(self.drawLineChart(procrank.drawingData(procrank.peakMediumProcs())))
+        story.append(Spacer(1, 0.6 * inch))
+        story.append(Paragraph('PSS Peak Below %s MB' % procrank.RAMS[procrank.ram][0], Styles['Normal']))
+        story.append(Spacer(1, 0.1 * inch))
+        story.append(self.drawLineChart(procrank.drawingData(procrank.peakLowProcs())))
+        story.append(Spacer(1, 0.6 * inch))
         print '---> Done'
         print '---> Start generate report in PDF ...'
         doc.build(story, onFirstPage=self.drawCoverPage, onLaterPages=self.drawContentPage)
